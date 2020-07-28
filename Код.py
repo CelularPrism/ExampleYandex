10df4a34f6b3d39463a5f1eed9b1d23bbc2ebfcf
HitakiNagato <kosta20135@mail.ru>
2020-07-28 20:07:07 +0700

    first commit

---
+++ Код.py
@@ -0,0 +1,26 @@
+import sys
+
+s1 = list(sys.stdin.readline().strip())
+s2 = list(sys.stdin.readline().strip())
+
+C = {}
+D = {}
+a = 1
+
+if len(s1) == len(s2):
+  for i in range(0, len(s1)):
+    C[s1[i]] = 0
+    D[s2[i]] = 0
+
+  for i in range(0, len(s1)):
+    C[s1[i]] += 1
+    D[s2[i]] += 1
+
+  if len(s1) == len(s2):
+    for i in range(0, len(s1)):
+      if C[s1[i]] != D[s1[i]]:
+        a = 0
+else:
+  a = 0
+
+print(a)
